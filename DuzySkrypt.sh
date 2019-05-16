612bfff7036f753ac8877758fecb17f6f1a0bbbf
JanDorniak <45369651+JanDorniak@users.noreply.github.com>
2019-05-16 23:56:43 +0200

    Add files via upload

--- DuzySkrypt.sh
+++ DuzySkrypt.sh
@@ -2,15 +2,15 @@
 # Author           : Jan Dorniak
 # Created On       : 6-05-2019
 # Last Modified By : Jan Dorniak
-# Last Modified On : 15-05-2019
-# Version          : v0.2
+# Last Modified On : 16-05-2019
+# Version          : v1.0
 #
-# Description      : Program umożliwia różne operacje na archiwizacji plików.
+# Description      : Skrypt umożliwia różne operacje na archiwizacji plików.
 # Opis
 #
 # Licensed under GPL (see /usr/share/common-licenses/GPL for more details
 # or contact # the Free Software Foundation for a copy)
-
+WERSJA="v1.0, autor: Jan Dorniak 175959"
 #1. naglowek
 #2. komentarze
 #3. Odporny na...
@@ -18,69 +18,145 @@
 #5. Bloko kodu w funkcjach
 #6. Pliki tymczasowe w katalogach
 
+#tymczasowe pliki na zapisywanie wyborow z dialogu
 INPUT=/tmp/input.sh.$$
 INPUT2=/tmp/input2.sh.$$
+INPUT3=/tmp/input3.sh.$$
 
-#find . -not -path '*/\.*' -type f
-
-OPCJEMENU=(1 "Wypakuj"
+OPCJEMENU=(1 "Wypakuj / Podejrzyj zawartość"
 	   	   2 "Spakuj"
            3 "Zmień bieżącą ścieżkę"
            4 "Wyświetl zawartość aktualnego folderu"
 		   5 "Wyjdź")
 
+function help()
+{
+	echo "Pomoc "
+	echo "----------------"
+	echo "1.Wypakowywanie:"
+	echo "	Po wybraniu tej opcji proszeni jesteśmy o wybranie pliku, który chcemy"
+	echo "	wypakować/podejrzeć. Nastepnie wpisujemy docelową ścieżkę wypakowania."
+	echo "	Jeśli chcemy wypakować w bieżącym klikamy anuluj. Następnie jesteśmy"
+	echo "	pytani, czy chcemy nadpisywać pliki o tej samej nazwie. Po odpowiedzi"
+	echo "	możemy wybrać czy wypakować plik czy tylko podejrzeć zawartość"
+	echo "----------------"
+	exit
+}
+
 function rozpakuj()
 {
-	#PLIKI=('find . -not -path '*/\.*' -type f')
+	readarray -d '' PLIKI < <(find . -not -path '*/\.*' -type f -maxdepth 1 -print0) 
 
-	readarray -d '' PLIKI < <(find . -not -path '*/\.*' -type f -print0) #zeby nie szukalo w innych folderach
-
-	IT=1
+	IT=1 #index 
 	SPIS=()
 
-	for i in "${PLIKI[@]}"
+	#przeformatowanie listy plikow na odpowiednia do menu
+	for i in "${PLIKI[@]}" 
 	do
 		SPIS+=($IT "$i")
 		IT=$((IT+1))
 	done
 
-	#clear
-	#echo "${SPIS[@]}"
-	#sleep 10
-
-	dialog --title "Wybierz plik na którym chcesz operować (spacją)" --backtitle "tytul" \
-	--menu "menu" 0 0 0 "${SPIS[@]}"
-
-	#sprawdzenie rozszerzenia
-
-	dialog --title "Co chcesz zrobić?" \
-	--menu "Wybierz:" 0 0 0 \
-	"1" "Wypakować" \
-	"2" "Wyświetlić zawartość" \
-	"3" "Wyświetlić komentarz archiwum" 2>"${INPUT2}"
-}
-
-
-function zmien_sciezke()
-{
-	dialog --title "Jestes w $PWD" --backtitle "Podaj ścieżkę" \
-	--inputbox "Podaj docelową scieżkę" 0 0 2>"${INPUT2}"
+	#wybranie pliku
+	dialog --title "Wybierz plik na którym chcesz operować" --backtitle "Wybór pliku" \
+	--menu "Wybierz:" 0 0 0 "${SPIS[@]}" 2>"${INPUT2}"
 
 	if [ $? -eq 1 ]; then #jesli kliknieto anuluj
 		return;
 	fi
 
-	FOLDER=$(<"${INPUT2}")
+	#folder docelowy
+	zmien_sciezke
+	SCIEZKA=$FOLDER
 
-	cd $FOLDER
+	INDEX=$(<"${INPUT2}")
+	INDEX=$((INDEX-1))
+	PLIK="${PLIKI[INDEX]}"
 
-	return $FOLDER #!!!!
+	#czy nadpisywac
+	dialog --title "Czy nadpisywać?" --yesno "Czy nadpisywać pliki?" 0 0
+
+	NADPIS=$?
+	NADPISTAR=""
+	NADPISZIP="-o"
+	NADPISZRAR="-o+"
+	if [ $NADPIS -eq 1 ]; then
+		NADPISTAR="-k"
+		NADPISZIP="-f"
+		NADPISZRAR="-o-"
+	fi
+
+	#sprawdzenie poprawnosci formatu pliku
+	if [ ${PLIK: -4} == ".tar" ]; then
+		WYPAKUJ="tar -xf $PLIK -C $SCIEZKA $NADPISTAR"
+		ZAWARTOSC="tar -tvf $PLIK"
+	elif [ ${PLIK: -7} == ".tar.gz" ]; then
+		WYPAKUJ="tar -zxf $PLIK --directory $SCIEZKA $NADPISTAR"
+		ZAWARTOSC="tar -ztvf $PLIK"
+	elif [ ${PLIK: -8} == ".tar.bz2" ]; then
+		WYPAKUJ="tar -jxf $PLIK --directory $SCIEZKA $NADPISTAR"
+		ZAWARTOSC="tar -jtvf $PLIK"
+	elif [ ${PLIK: -4} == ".zip" ]; then
+		WYPAKUJ="unzip $NADPISZIP $PLIK -d $SCIEZKA"
+		ZAWARTOSC="unzip -l $PLIK"
+	elif [ ${PLIK: -4} == ".rar" ]; then
+		WYPAKUJ="unrar x $NADPISZRAR $PLIK $SCIEZKA"
+		ZAWARTOSC="unrar l $PLIK"
+	else
+		dialog --msgbox "ZLY FORMAT PLIKU!" 0 0
+		return
+	fi
+
+	dialog --clear --title "Co chcesz zrobić?" \
+	--menu "Wybierz:" 0 0 0 \
+	1 "Wypakować" \
+	2 "Wyświetlić zawartość i komentarz" 2>"${INPUT2}"
+
+	if [ $? -eq 1 ]; then #jesli kliknieto anuluj
+		return;
+	fi
+
+	WYBOR=$(<"${INPUT2}")
+
+	case $WYBOR in 
+		1)
+			$WYPAKUJ
+			;;
+		2)
+			WARTOSCI=$($ZAWARTOSC)
+			dialog --msgbox "$WARTOSCI" 0 0
+			;;
+	esac
 }
 
-function wyswietl_folder()
+function spakuj()
 {
-	dialog --
+	ZMIENNA=()
 }
+
+function zmien_sciezke()
+{
+	dialog --title "Aktualna ścieżka $PWD" --backtitle "Podaj ścieżkę" \
+	--inputbox "Wpisz ścieżkę docelową, anuluj pozostawia aktualną ścieżkę" 0 0 2>"${INPUT3}"
+
+	if [ $? -eq 1 ]; then #jesli kliknieto anuluj
+		FOLDER=$PWD
+		return
+	fi
+
+	FOLDER=$(<"${INPUT3}")
+}
+
+
+#sprawdzenie argumentów wywołania
+if [ $1 == '-h' ]; then
+	help
+	exit
+elif [ $1 == '-v' ]; then
+	echo $WERSJA
+	exit
+fi
+
 
 while true; do
 
@@ -91,13 +167,14 @@ while true; do
 
 	case $WYBOR in 
 		1)
-			rozpakuj
+			rozpakuj #zrobione
 			;;
 		2)
-			echo "Opcja 2"
+			spakuj
 			;;
 		3) 	
 			zmien_sciezke #zrobione
+			cd $FOLDER
 			;;
 		4)
 			ZAWARTOSC=$(ls) #zrobione #do zrobienia rozmiary 
@@ -114,4 +191,5 @@ clear
 
 [ -f $INPUT ] && rm $INPUT
 [ -f $INPUT2 ] && rm $INPUT2
+[ -f $INPUT3 ] && rm $INPUT3
 
